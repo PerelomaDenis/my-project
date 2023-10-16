26c87c141856106cf917423929c8789ded804342
DenisPereloma <den.pereloma@gmail.com>
2023-10-16 20:22:52 +0300

    add error boundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+    className?: string;
+}
+
+export function PageError(props: PageErrorProps) {
+    const { className } = props;
+
+    const { t } = useTranslation();
+
+    const onReload = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <div className={cls.title}>{t('Возникла непредвиденная ошибка')}</div>
+            <Button onClick={onReload}>{t('Обновить страницу')}</Button>
+        </div>
+    );
+}
