b48e1d46cab8490bdbf9d192b13bcecf0e327619
DenisPereloma <den.pereloma@gmail.com>
2023-10-16 20:29:52 +0300

    add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export const buildPlugins = (options: BuildOptions): webpack.WebpackPluginInstance[] => {
@@ -17,6 +18,9 @@ export const buildPlugins = (options: BuildOptions): webpack.WebpackPluginInstan
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 };
