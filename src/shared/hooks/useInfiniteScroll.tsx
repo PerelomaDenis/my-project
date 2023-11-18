a9666f83fea7321b38ac08d94e04633bb34ad8ab
Перелома Денис <denis.pereloma@gazprombank.ru>
2023-11-18 21:38:09 +0300

    add infinite scroll

--- src/shared/hooks/useInfiniteScroll.tsx
+++ src/shared/hooks/useInfiniteScroll.tsx
@@ -10,10 +10,12 @@ export const useInfiniteScroll = (options: UseInfiniteScrollOptions) => {
     const { wrapperRef, triggerRef, callback } = options;
 
     useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
         let observer: IntersectionObserver | null = null;
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -24,13 +26,13 @@ export const useInfiniteScroll = (options: UseInfiniteScrollOptions) => {
                 }
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
         }
 
         return () => {
-            if (observer) {
+            if (observer && triggerElement) {
                 // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(triggerRef.current);
+                observer.unobserve(triggerElement);
             }
         };
     }, [callback, triggerRef, wrapperRef]);
